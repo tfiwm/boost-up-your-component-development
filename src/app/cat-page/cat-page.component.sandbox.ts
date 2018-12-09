450ecb521f4971ca7fb1b0dcd5846e6fd2b8a750
Mitko Tschimev <mitko.tschimev@icloud.com>
2018-12-09 16:50:24 +0100

    feat(): add ngxs mocks

--- src/app/cat-page/cat-page.component.sandbox.ts
+++ src/app/cat-page/cat-page.component.sandbox.ts
@@ -4,12 +4,14 @@ import { MatCardModule, MatIconModule, MatButtonModule, MatToolbarModule } from 
 import { CatCardComponent } from '../cat-card/cat-card.component';
 import { CatToolbarComponent } from '../cat-toolbar/cat-toolbar.component';
 import { CatListComponent } from '../cat-list/cat-list.component';
-import { Cat } from '../model/cat.model';
 import { CatPageService } from './cat-page.service';
 import { throwError } from 'rxjs';
+import { NgxsModule, Store } from '@ngxs/store';
+import { AppState } from '../state/app.state';
+import { PlaygroundSetupService } from 'src/playground-helper';
 
 export default sandboxOf(CatPageComponent, {
-  imports: [MatCardModule, MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [MatCardModule, MatIconModule, MatButtonModule, MatToolbarModule, NgxsModule.forFeature([AppState])],
   declarations: [CatToolbarComponent, CatListComponent, CatCardComponent],
   providers: [CatPageService]
 })
@@ -17,13 +19,23 @@ export default sandboxOf(CatPageComponent, {
     template: `<app-cat-page></app-cat-page>`
   })
   .add('error loading cats', {
-    template: `<app-cat-page></app-cat-page>`,
+    template: `
+      <app-cat-page></app-cat-page>
+      <app-playground-setup></app-playground-setup>
+    `,
     providers: [
       {
-        provide: CatPageService,
-        useValue: {
-          getCats: () => throwError('Soem error')
-        }
+        provide: PlaygroundSetupService,
+        useFactory: (store: Store) => {
+          store.reset({
+            app: {
+              deleteMode: false,
+              catsList: [],
+              errorLoadingCatsList: true
+            }
+          });
+        },
+        deps: [Store]
       }
     ]
   });
